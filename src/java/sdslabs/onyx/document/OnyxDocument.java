115dd0f6eee42fac84e720e0744edb34d9417cbc
kumarishan <kumar.ishan.opensource@gmail.com>
2011-01-24 16:47:43 +0530

    modified:   OnyxDocument.java

--- src/java/sdslabs/onyx/document/OnyxDocument.java
+++ src/java/sdslabs/onyx/document/OnyxDocument.java
@@ -1,3 +1,11 @@
 package sdslabs.onyx.document;
 
-interface O
+interface OnyxDocument {
+
+  public void addDocument(File file);
+  public void addMetaData(HashMap<String,String> metadata);
+  public Document getDocument();
+  public HashMap<String,String> getMetaData();
+  public String getType();
+
+}
