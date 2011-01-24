3d14826efdc0dfec2a31dd6e33617c40af51eca8
kumarishan <kumar.ishan.opensource@gmail.com>
2011-01-24 16:38:54 +0530

    new file:   OnyxDocumentDirectory.java 	modified:   OnyxPDFDocument.java 	modified:   ../indexer/OnyxIndexer.java

---
+++ src/java/sdslabs/onyx/document/OnyxDocumentDirectory.java
@@ -0,0 +1,23 @@
+package sdslabs.onyx.document;
+
+
+public class DocumentDirectory {
+
+  
+  public void addDocument( Document doc ){
+  }
+
+  public Document getDocument( int index ){
+  }
+
+  public Document iterateDocument(){
+
+  }
+  
+  public addDirectory(){
+  
+  }
+
+}
+
+
