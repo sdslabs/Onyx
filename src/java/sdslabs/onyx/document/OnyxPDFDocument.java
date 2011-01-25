1e1c7e5205e77404d772f33317ba6534f7050ac3
kumarishan <kumar.ishan.opensource@gmail.com>
2011-01-25 05:39:39 +0530

    modified:   OnyxPDFDocument.java

--- src/java/sdslabs/onyx/document/OnyxPDFDocument.java
+++ src/java/sdslabs/onyx/document/OnyxPDFDocument.java
@@ -1,2 +1,32 @@
 package sdslabs.onyx.document;
 
+class OnyxPDFDocument {
+
+
+  public OnyxPDFDocument(){
+
+  }
+
+  public void addDocument(File file){
+
+  }
+
+  public void addMetaData(HashMap<String,String> metadata){
+
+  }
+
+  public Document getDocument(){
+  }
+
+  public HashMap<String,String> getMataData(){
+
+  }
+
+  public String getType(){
+    return "PDF";
+  }
+
+
+ 
+}
+
