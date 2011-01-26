16b91e64247661f2325a3ae1b523f28cadbebdb1
kumarishan <kumar.ishan.opensource@gmail.com>
2011-01-27 04:16:28 +0530

    modified:   OnyxDocumentDirectory.java

--- src/java/sdslabs/onyx/document/OnyxDocumentDirectory.java
+++ src/java/sdslabs/onyx/document/OnyxDocumentDirectory.java
@@ -1,23 +1,31 @@
 package sdslabs.onyx.document;
 
 
-public class DocumentDirectory {
+public class OnyxDocumentDirectory {
 
+  private List<OnyxDocument> documents;
+  private int lastIndex;
+
+  public OnyxDocumentDirectory(){
+    documents = new HashMap<OnyxDocument>();  
+    lastIndex = -1;
+  }
+
+  public void addDocument( OnyxDocument doc ){
+    documents.add(doc);
+  }
+
+  public OnyxDocument getDocument( int index ){
+    return documents.get(i);
+  }
+
+  public OnyxDocument iterateDocument(){
+    lastIndex++;
+    if(lasIndex == documents.size())
+      return null;
+    return documents.get(lastIndex);
+  }
   
-  public void addDocument( Document doc ){
-  }
-
-  public Document getDocument( int index ){
-  }
-
-  public Document iterateDocument(){
-
-  }
-  
-  public addDirectory(){
-  
-  }
-
 }
 
 
